5a616b08b68bbd684ac5c2badd6ca638309f87d8
Oleg-Luchnikov <oleg19931208@gmail.com>
2016-07-10 15:05:27 +0300

    new project

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
